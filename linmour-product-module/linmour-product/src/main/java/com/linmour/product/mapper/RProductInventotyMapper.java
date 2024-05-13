4a64fab50ee798a2393c0c8aca5a9fa484dc4a9a
linmour <2192177945@qq.com>
2024-05-13 16:55:00 +0800

    缺失文件

---
+++ linmour-product-module/linmour-product/src/main/java/com/linmour/product/mapper/RProductInventotyMapper.java
@@ -0,0 +1,18 @@
+package com.linmour.product.mapper;
+
+import com.linmour.product.pojo.Do.RProductInventoty;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author linmour
+* @description 针对表【r_product_inventoty】的数据库操作Mapper
+* @createDate 2024-04-26 12:25:04
+* @Entity com.linmour.product.pojo.Do.RProductInventoty
+*/
+public interface RProductInventotyMapper extends BaseMapper<RProductInventoty> {
+
+}
+
+
+
+
